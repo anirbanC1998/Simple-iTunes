5714f98c300e7d1732057e2439e710f185466d5f
Shane Park <seyeon9897@gmail.com>
2020-02-12 18:26:30 -0500

    setup GUI

--- src/app/SongLib.java
+++ src/app/SongLib.java
@@ -1,34 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package app;
-
-import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import view.ListController;
 
+/**
+ *
+ * @author shanepark
+ */
 public class SongLib extends Application {
-	@Override
-	public void start(Stage primaryStage) 
-	throws IOException {
-		FXMLLoader loader = new FXMLLoader();   
-		loader.setLocation(
-				getClass().getResource("/view/List.fxml"));
-		AnchorPane root = (AnchorPane)loader.load();
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/List.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
 
-
-		ListController listController = loader.getController();
-		listController.start(primaryStage);
-
-		Scene scene = new Scene(root, 200, 300);
-		primaryStage.setScene(scene);
-		primaryStage.show(); 
-	}
-
-	public static void main(String[] args) {
-		launch(args);
-	}
-
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
 }
--- src/view/ListController.java
+++ src/view/ListController.java
@@ -1,92 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package view;
 
-import java.util.Optional;
-
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TextInputDialog;
-import javafx.stage.Stage;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
-public class ListController {
-
-	@FXML         
-	ListView<String> listView;                
-
-	private ObservableList<String> obsList;              
-
-	public void start(Stage mainStage) {                
-		// create an ObservableList 
-		// from an ArrayList  
-		obsList = FXCollections.observableArrayList(                               
-				"Giants",                               
-				"Patriots",
-				"49ers",
-				"Rams",
-				"Packers",
-				"Colts",
-				"Cowboys",
-				"Broncos",
-				"Vikings",
-				"Dolphins",
-				"Titans",
-				"Seahawks",
-				"Steelers",
-				"Jaguars"); 
-
-		listView.setItems(obsList); 
-		
-		// select the first item
-	      listView.getSelectionModel().select(0);
-
-	      // set listener for the items
-	      listView
-	        .getSelectionModel()
-	        .selectedIndexProperty()
-	        .addListener(
-	           (obs, oldVal, newVal) -> 
-	               showItemInputDialog(mainStage));
-
-	}
-	
-	private void showItem(Stage mainStage) {                
-	      Alert alert = 
-	         new Alert(AlertType.INFORMATION);
-	      //alert.initModality(Modality.NONE);
-	      alert.initOwner(mainStage);
-	      alert.setTitle("List Item");
-	      alert.setHeaderText(
-	           "Selected list item properties");
-
-	      String content = "Index: " + 
-	          listView.getSelectionModel()
-	                   .getSelectedIndex() + 
-	          "\nValue: " + 
-	          listView.getSelectionModel()
-	                   .getSelectedItem();
-
-	          alert.setContentText(content);
-	          alert.showAndWait();
-	          // alert.show();  
-	          //System.out.println("hey there!");
-	          
-	   }
-	
-	private void showItemInputDialog(Stage mainStage) {                
-	      String item = listView.getSelectionModel().getSelectedItem();
-	      int index = listView.getSelectionModel().getSelectedIndex();
-
-	      TextInputDialog dialog = new TextInputDialog(item);
-	      dialog.initOwner(mainStage); dialog.setTitle("List Item");
-	      dialog.setHeaderText("Selected Item (Index: " + index + ")");
-	      dialog.setContentText("Enter name: ");
-
-	      Optional<String> result = dialog.showAndWait();
-	      if (result.isPresent()) { obsList.set(index, result.get()); }
-	   }
-
-
+/**
+ *
+ * @author shanepark
+ */
+public class ListController implements Initializable {
+    
+    @FXML private Label songLibraryLabel;
+    @FXML private Label selectedSongLabel;
+    @FXML private Label newSongLabel;
+    
+    @FXML ListView<String> listView;
+    
+    @FXML
+    private void handleAdd(ActionEvent event) {
+        System.out.println("You clicked me!");
+//        label.setText("Hello World!");
+    }
+    @FXML
+    private void handleEdit(ActionEvent event) {
+        System.out.println("You clicked me!");
+    }
+    @FXML
+    private void handleDelete(ActionEvent event) {
+        System.out.println("You clicked me!");
+    }
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        songLibraryLabel.setText("Song Library");
+        selectedSongLabel.setText("Selected Song's Info");
+        newSongLabel.setText("New Song's Info");
+        songLibraryLabel.setFont(Font.font("Verdana", FontWeight.BOLD, 12));
+        selectedSongLabel.setFont(Font.font("Verdana", FontWeight.BOLD, 15));
+        newSongLabel.setFont(Font.font("Verdana", FontWeight.BOLD, 15));
+    }    
+    
 }
